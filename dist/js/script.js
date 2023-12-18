ed0dba50ef97064e292a7cc31bdee7d524245620
Anna Burnos <burnos.anna@gmail.com>
2023-12-18 11:52:54 +0100

    Add code to Cart.update method which updates html (prices, fees and amounts)

--- dist/js/script.js
+++ dist/js/script.js
@@ -375,7 +375,9 @@ const select = {
     announce(){
       const thisWidget = this;
 
-      const event = new Event('updated');
+      const event = new CustomEvent('updated', {
+        bubbles: true
+      });
       thisWidget.element.dispatchEvent(event);
     }
   }
@@ -397,7 +399,11 @@ const select = {
 
       thisCart.dom.wrapper = element;
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
-      thisCart.dom.productList = this.dom.wrapper.querySelector(select.cart.productList);
+      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
+      thisCart.dom.deliveryFee = thisCart.dom.wrapper.querySelector(select.cart.deliveryFee);
+      thisCart.dom.subtotalPrice = thisCart.dom.wrapper.querySelector(select.cart.subtotalPrice);
+      thisCart.dom.totalPrice = thisCart.dom.wrapper.querySelectorAll(select.cart.totalPrice);
+      thisCart.dom.totalNumber = thisCart.dom.wrapper.querySelector(select.cart.totalNumber);
     }
 
     initActions(){
@@ -405,6 +411,10 @@ const select = {
       thisCart.dom.toggleTrigger.addEventListener('click', function(event){
         event.preventDefault();
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+
+      thisCart.dom.productList.addEventListener('updated', function(){
+        thisCart.update();
       });
     }
 
@@ -450,6 +460,19 @@ const select = {
       console.log('subtotalPrice:', thisCart.subtotalPrice);
       console.log('deliveryFee:', deliveryFee);
       console.log('totalPrice:', thisCart.totalPrice);
+
+      thisCart.dom.totalNumber.innerHTML = thisCart.totalNumber;
+      thisCart.dom.subtotalPrice.innerHTML = thisCart.subtotalPrice;
+
+      if(thisCart.subtotalPrice > 0){
+        thisCart.dom.deliveryFee.innerHTML = deliveryFee;
+      }else{
+        thisCart.dom.deliveryFee.innerHTML = 0;
+      }
+
+      thisCart.dom.totalPrice.forEach(element => {
+        element.innerHTML = thisCart.totalPrice;
+      });
     }
   }
 
